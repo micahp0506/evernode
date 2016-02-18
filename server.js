7c9286e73a7ba1d2c8aa6afba3e6361d0bb17b3e
Micah Wells <micahp0506@gmail.com>
2016-02-18 10:35:14 -0600

    New note form feature completed

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 'use strict'
 
 
-
+const bodyParser = require('body-parser')
 const express = require('express');
 
 const app = express();
+// Port where our app will run
 const PORT = process.env.PORT || 3000;
+
+// Allows us to use jade, this before routes
+app.set('view engine', 'jade')
+// Middleware for using bodyParser
+app.use(bodyParser.urlencoded({
+    exended:false
+}));
 
 app.get('/', (req, res) => {
     res.send("She's lost that loving feeling");
+});
+
+app.get('/notes/new', (req, res) => {
+    res.render('new-note');
+});
+
+app.post('/notes', (req,res) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.listen(PORT, () => {
